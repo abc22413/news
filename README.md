bfb20523a2e7496364d65cf8782bbd6a9305af80
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2019-11-28 23:19:22 +0800

    added readme.md

---
+++ README.md
@@ -0,0 +1,8 @@
+TODO:-
+- Security config
+- Referrer policy
+- CSP
+- Recaptcha for acc making
+- Email confirmation
+- Make admin page nicer
+- Add list_filter to admin page
--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Article)
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Comment)
