ff6b5a7c2139f8c87e54d215727959885e195089
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2019-11-29 10:46:23 +0800

    CSP go

--- README.md
+++ README.md
@@ -2,9 +2,7 @@ TODO:-
 - Link to details page
 - Comments: Add comments framework
 - User age-limit
-- SECURITY: MAKE CSP WORK
 - Recaptcha for acc making
 - Email confirmation
-- Profanities Filtering
 - Make admin page nicer
 - Add list_filter to admin page
--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    #'csp.middleware.CSPMiddleware',
+    'csp.middleware.CSPMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
