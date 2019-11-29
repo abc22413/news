2692f581761b7b2b8fe0db54ddeaaa0f376db2bd
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2019-11-29 11:25:59 +0800

    adding sentry error handling

--- users/views.py
+++ users/views.py
@@ -6,3 +6,13 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+#FOR SENTRY ERROR REPORTING
+from sentry_sdk import last_event_id
+from django.shortcuts import render
+
+def handler500(request, *args, **argv):
+    return render(request, "500.html", {
+        'sentry_event_id': last_event_id(),
+    }, status=500)
