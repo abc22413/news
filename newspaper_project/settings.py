c25c856e3dd4af0440e59c304559cf611c77ca6d
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2019-11-30 14:37:38 +0800

    Update settings.py

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"templates/css"),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Sentry.io integration
 import sentry_sdk
