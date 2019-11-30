90d74f441381e410fbab8fccd73004a8a4ed4d83
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2019-11-30 15:15:29 +0800

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,8 @@
 from django import forms
 from django.contrib.auth.forms import *
 from .models import *
+from captcha.fields import ReCaptchaField
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
