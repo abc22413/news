980be3ab7b30255e6e2220e0a761f68d1a9ec665
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2019-11-30 11:29:45 +0800

    copied static files where they needed ot be

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates')
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Sentry.io integration
