0435f5e5e76a211aa7bc400c9a8458e96d0ab69c
abc22413 <smrtgoaheadsbstowertransit@gmail.com>
2019-11-30 14:46:19 +0800

    Added nice 404

--- pages/views.py
+++ pages/views.py
@@ -12,3 +12,6 @@ def handler500(request, *args, **argv):
     return render(request, "500.html", {
         'sentry_event_id': last_event_id(),
     }, status=500)
+
+def handler404(request, *args, **argv):
+    return render(request, "404.html", status=404)
